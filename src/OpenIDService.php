dedc146487a82d685abe040b4792e3d62661cc00
Marko Harjula <marko.harjula@gmail.com>
2016-10-07 11:15:18 +0300

    Update OpenIDService.php

--- src/OpenIDService.php
+++ src/OpenIDService.php
@@ -67,8 +67,8 @@ abstract class OpenIDService implements OpenIDServiceInterface {
 		}
 		curl_setopt($this->ch, CURLOPT_PROXY, $this->openIdConfig->getHttpProxy() );
 		if ( $post == null ) {
+			curl_setopt($this->ch, CURLOPT_POSTFIELDS,array());
 			curl_setopt($this->ch, CURLOPT_POST, 0);
-			curl_setopt($this->ch, CURLOPT_POSTFIELDS,null);
 		} else  {
 			curl_setopt($this->ch, CURLOPT_POST, 1);
 			if ( is_array($post) ) {
